3e4f72f12ec1eafbbb6dfb18f469ff6597f7818c
jsk8702 <jsk8702@naver.com>
2022-02-28 01:51:22 +0900

    22.02.28
    up and down button adding

--- js/script.js
+++ js/script.js
@@ -36,39 +36,99 @@ document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function () {
 
     //위로 가기 버튼이 첫 화면 내려가면 나오고 그 다음부터 누르면 맨위로 간다.
-    $(window).scroll(function () {
-        //화면 상단의 스크롤 값을 알아본다.
-        var ht = $(this).scrollTop();
-        //        console.log(sc);
+    // $(window).scroll(function () {
+    //화면 상단의 스크롤 값을 알아본다.
+    // var ht = $(this).scrollTop();
+    //        console.log(sc);
 
-        // 화면 하단의 스크롤 값을 알아본다.
-
-
+    // 화면 하단의 스크롤 값을 알아본다.
 
 
-        //화면 스크롤이 200 이상 내려가면 네비게이션이 탑에 고정된다.
-        if (ht > 469) {
-            $('header .head_btn_up').removeClass('on');
-            $('header .head_btn_down').removeClass('on');
-            $('header .head_btn_up').addClass('on');
-            $('header .head_btn_down').addClass('on');
-        };
-        //화면 스크롤이 200 이상 올라가면 네비게이션이 원 위치로 돌아간다.
-        if (ht < 469) {
-            $('header .head_btn_up').removeClass('on');
-            $('header .head_btn_down').removeClass('on');
-        };
-    });
+
+
+    //화면 스크롤이 200 이상 내려가면 네비게이션이 탑에 고정된다.
+    // if (ht > 469) {
+    //     $('header .head_btn_up').removeClass('on');
+    //     $('header .head_btn_down').removeClass('on');
+    //     $('header .head_btn_up').addClass('on');
+    //     $('header .head_btn_down').addClass('on');
+    // };
+    //화면 스크롤이 200 이상 올라가면 네비게이션이 원 위치로 돌아간다.
+    // if (ht < 469) {
+    //     $('header .head_btn_up').removeClass('on');
+    //     $('header .head_btn_down').removeClass('on');
+    // };
+    // });
 
     //위로 가기 버튼을 클릭하면 사이트 탑으로 이동한다.
-    $('header .head_btn_up').click(function (e) {
-        //a태그 성질을 죽여서 누르면 화면 위로 이동하는 문제해결
-        e.preventDefault();
+    // $('header .head_btn_up').click(function (e) {
+    //a태그 성질을 죽여서 누르면 화면 위로 이동하는 문제해결
+    // e.preventDefault();
 
-        var ht = $('header').height();
-        //그냥 ht 하면 header 아래에 온다. header의 전체높이 + section 의 padding-top 200을 계산해서 화면 위로 가라고 -200을 줬다.
-        $('html, body').stop().animate({ 'scrollTop': ht - 200 }, 800);
+    // var ht = $('header').height();
+    //그냥 ht 하면 header 아래에 온다. header의 전체높이 + section 의 padding-top 200을 계산해서 화면 위로 가라고 -200을 줬다.
+    // $('html, body').stop().animate({ 'scrollTop': ht - 200 }, 800);
+    // });
+
+
+    // 상하단 버튼 설정
+    /******* 스크롤 감지하여 버튼이 나타나고 사라지게 합니다 ******/
+    $(window).scroll(function () {
+
+        /* 
+            현재 스크롤이 최상단으로부터 조금 내려왔을 경우
+            UP 버튼을 나타나게 합니다.
+        */
+        if ($(this).scrollTop() > 100) {
+            $('#upBtn').fadeIn();
+        }
+
+        /* 
+            현재 스크롤이 최상단 기준으로 올라왔을 경우
+            UP 버튼을 사라지게 합니다.
+        */
+        else {
+            $('#upBtn').fadeOut();
+        }
+
+        /* 
+            현재 스크롤이 최하단으로부터 조금 올라왔을 경우
+            DOWN 버튼을 나타나게 합니다.
+        */
+        if ($(this).scrollTop() < ($(document).height() - $(window).height() - 100)) {
+            $('#downBtn').fadeIn();
+        }
+
+        /* 
+            현재 스크롤이 최하단 기준으로 내려왔을 경우
+            DOWN 버튼을 사라지게 합니다.
+        */
+        else {
+            $('#downBtn').fadeOut();
+        }
+
     });
+    /*****************************************************************/
+
+
+    /* 
+        UP버튼 클릭 시 자연스럽게 최상단으로 올라가는 효과를 줍니다.
+        400 -> 0.4초 
+    */
+    $('#upBtn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 400);
+        return false;
+    });
+
+    /* 
+        DOWN버튼 클릭 시 자연스럽게 최하단으로 내려가는 효과를 줍니다. 
+    */
+    $('#downBtn').click(function () {
+        $('html, body').animate({ scrollTop: $(document).height() }, 400);
+        return false;
+    });
+
+
 
 
     //헤더 메뉴를 누르면 해당 section이 나온다.
