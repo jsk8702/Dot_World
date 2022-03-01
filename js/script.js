d32993ca886a49c3ac7c958c3036e64447a6981a
jsk8702 <jsk8702@naver.com>
2022-03-02 00:21:43 +0900

    22.03.02
    section adding

--- js/script.js
+++ js/script.js
@@ -35,42 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
 // 기타 제이쿼리 설정
 $(document).ready(function () {
 
-    //위로 가기 버튼이 첫 화면 내려가면 나오고 그 다음부터 누르면 맨위로 간다.
-    // $(window).scroll(function () {
-    //화면 상단의 스크롤 값을 알아본다.
-    // var ht = $(this).scrollTop();
-    //        console.log(sc);
-
-    // 화면 하단의 스크롤 값을 알아본다.
-
-
-
-
-    //화면 스크롤이 200 이상 내려가면 네비게이션이 탑에 고정된다.
-    // if (ht > 469) {
-    //     $('header .head_btn_up').removeClass('on');
-    //     $('header .head_btn_down').removeClass('on');
-    //     $('header .head_btn_up').addClass('on');
-    //     $('header .head_btn_down').addClass('on');
-    // };
-    //화면 스크롤이 200 이상 올라가면 네비게이션이 원 위치로 돌아간다.
-    // if (ht < 469) {
-    //     $('header .head_btn_up').removeClass('on');
-    //     $('header .head_btn_down').removeClass('on');
-    // };
-    // });
-
-    //위로 가기 버튼을 클릭하면 사이트 탑으로 이동한다.
-    // $('header .head_btn_up').click(function (e) {
-    //a태그 성질을 죽여서 누르면 화면 위로 이동하는 문제해결
-    // e.preventDefault();
-
-    // var ht = $('header').height();
-    //그냥 ht 하면 header 아래에 온다. header의 전체높이 + section 의 padding-top 200을 계산해서 화면 위로 가라고 -200을 줬다.
-    // $('html, body').stop().animate({ 'scrollTop': ht - 200 }, 800);
-    // });
-
-
     // 상하단 버튼 설정
     /******* 스크롤 감지하여 버튼이 나타나고 사라지게 합니다 ******/
     $(window).scroll(function () {
@@ -144,6 +108,54 @@ $(document).ready(function () {
 
         //탭 메뉴 색상이 클릭하면 변경된다.
         $('.header_nav > li').removeClass('on');
+        $(this).addClass('on');
+    });
+
+    //act1 section의 번호를 누르면 해당 section이 나온다.
+    $('.act1 .section_btn ul li').click(function (e) {
+        e.preventDefault();
+
+        //섹션 번호메뉴의 li 인덱스 번호를 가져온다. 
+        var uNum2 = $(this).index();
+
+        $('.act1 .section_inner').removeClass('active');
+
+        $('.act1 .section_inner').eq(uNum2).addClass('active');
+
+        //섹션 번호 메뉴를 선택한 번호의 색상이 바뀐다.
+        $('.section_btn > ul > li').removeClass('on');
+        $(this).addClass('on');
+    });
+
+    //act2 section의 번호를 누르면 해당 section이 나온다.
+    $('.act2 .section_btn ul li').click(function (e) {
+        e.preventDefault();
+
+        //섹션 번호메뉴의 li 인덱스 번호를 가져온다. 
+        var uNum2 = $(this).index();
+
+        $('.act2 .section_inner').removeClass('active');
+
+        $('.act2 .section_inner').eq(uNum2).addClass('active');
+
+        //섹션 번호 메뉴를 선택한 번호의 색상이 바뀐다.
+        $('.section_btn > ul > li').removeClass('on');
+        $(this).addClass('on');
+    });
+
+    //act3 section의 번호를 누르면 해당 section이 나온다.
+    $('.act3 .section_btn ul li').click(function (e) {
+        e.preventDefault();
+
+        //섹션 번호메뉴의 li 인덱스 번호를 가져온다. 
+        var uNum2 = $(this).index();
+
+        $('.act3 .section_inner').removeClass('active');
+
+        $('.act3 .section_inner').eq(uNum2).addClass('active');
+
+        //섹션 번호 메뉴를 선택한 번호의 색상이 바뀐다.
+        $('.section_btn > ul > li').removeClass('on');
         $(this).addClass('on');
     });
 
