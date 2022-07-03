1310fa954466537df7c4917a9a6570f2af2d7543
jsk8702 <jsk8702@naver.com>
2022-07-03 23:58:27 +0900

    2022.07.03
    2차 사이트 수정

--- site2/js/script.js
+++ site2/js/script.js
@@ -6,3 +6,19 @@ $(document).ready(function () {
 
     
 });
+
+// swpier 기본 설정
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
