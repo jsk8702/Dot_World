86ee71298d2a5aa1eb19f1c59b479c5fc6f8f4f3
jsk8702 <jsk8702@naver.com>
2023-04-16 23:57:22 +0900

    23.04.16
    site3 슬라이드 및 해상도 변경

--- site3/js/script.js
+++ site3/js/script.js
@@ -1,25 +1,101 @@
 // 자바스크립트 설정
 
 // 1. 이미지 슬라이드 효과(https://www.youtube.com/watch?v=-1mJGh627R4)
-let currSlide = 1;
-showSlide(currSlide);
+// 슬라이드1 설정
+let currSlide1 = 1;
+showSlide1(currSlide1);
 
-function button_click(num){
-  showSlide((currSlide += num));
+function button_click1(num){
+  showSlide1((currSlide1 += num));
 }
 
-function showSlide(num){
-  const slides = document.querySelectorAll(".slide");
+function showSlide1(num){
+  const slides = document.querySelectorAll(".slide1");
   if(num > slides.length){
-    currSlide = 1;
+    currSlide1 = 1;
   } if(num<1){
-    currSlide = slides.length;
+    currSlide1 = slides.length;
   }
 
   for(let i=0; i < slides.length; i++){
     slides[i].style.display="none";
-  }slides[currSlide - 1].style.display="block";
+  }slides[currSlide1 - 1].style.display="block";
 }
+
+
+// 슬라이드2 설정
+let currSlide2 = 1;
+showSlide2(currSlide2);
+
+function button_click2(num){
+  showSlide2((currSlide2 += num));
+}
+
+function showSlide2(num){
+  const slides = document.querySelectorAll(".slide2");
+  if(num > slides.length){
+    currSlide2 = 1;
+  } if(num<1){
+    currSlide2 = slides.length;
+  }
+
+  for(let i=0; i < slides.length; i++){
+    slides[i].style.display="none";
+  }slides[currSlide2 - 1].style.display="block";
+}
+
+
+// 이미지 로딩 지연(lazy loading) 설정
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages;    
+
+  if ("IntersectionObserver" in window) {
+    lazyloadImages = document.querySelectorAll(".lazy");
+    var imageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.src = image.dataset.src;
+          image.classList.remove("lazy");
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function(image) {
+      imageObserver.observe(image);
+    });
+  } else {  
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll(".lazy");
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+
+      lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+      }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  }
+});
+
 
 // 제이쿼리 설정
 $(document).ready(function () {
