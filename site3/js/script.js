edfae7b1fcc26bb6362630e81be70c4983751860
jsk8702 <jsk8702@naver.com>
2024-02-19 23:48:53 +0900

    24.02.19
    모던 도트 업데이트 사이트 2

--- site3/js/script.js
+++ site3/js/script.js
@@ -4,7 +4,61 @@
 
 
 
+// 제이 퀴리 안해도됩니다.
 
+// ​
+
+// const image = document.querySelector('.image');
+
+//  const slideWidth = document.querySelector('.slide').offsetWidth; 
+
+// if (image.width > slideWidth) { image.style.width = '90%'; }
+
+// ​
+
+// 이런식으로 해도 되고, 아래처럼 제이 쿼리를 써도되긴한데 맞는 방식으로 수정해서 사용하시면됩니다.
+
+// ​
+
+// ​
+
+// $(document).ready(function() {
+
+// const slideContainer = $('.slide-container');
+
+// const image = $('.image');
+
+// ​
+
+// function resizeImage() {
+
+// const slideWidth = slideContainer.width();
+
+// const imageWidth = image.width();
+
+// ​
+
+// if (imageWidth > slideWidth) {
+
+// image.css('width', '90%');
+
+// } else {
+
+// image.css('width', '100%');
+
+// }
+
+// }
+
+// ​
+
+// $(window).on('resize', resizeImage);
+
+// ​
+
+// resizeImage();
+
+// });
 
 
 // 제이쿼리 설정
